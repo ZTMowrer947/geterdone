31107f363f7f0ca960d815c03070b4363258621b
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-11-14 18:03:01 -0600

    Add stub TodoList resource

--- api/app.py
+++ api/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from resources.todo import Todo
+from resources.todo import Todo, TodoList
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Todo, '/todos', '/todos/<int:id>')
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<int:id>')
 
 if __name__ == '__main__':
     app.run()
--- api/resources/todo.py
+++ api/resources/todo.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource
 
 
-class Todo(Resource):
+class TodoList(Resource):
     def get(self):
         return []
+
+
+class Todo(Resource):
+    def get(self, id: int):
+        return {id: id}
