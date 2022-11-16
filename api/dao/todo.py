5f7c73dbc5da0207c503b0f333121349486c73a4
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-11-16 10:33:04 -0600

    Fill out mock implementation of todo dao

--- api/dao/todo.py
+++ api/dao/todo.py
@@ -6,9 +6,9 @@ class TodoDao:
         if todos is None:
             todos = []
         self.todos = todos
-        self._next_id_ = len(todos) + 1
+        self._next_id_ = max(map(lambda todo: todo.id, todos)) + 1
 
-    def get_list(self):
+    def get_list(self) -> list[Todo]:
         return self.todos
 
     def get_one(self, todo_id: int):
@@ -18,11 +18,29 @@ class TodoDao:
 
         return None
 
-    def create(self, new_todo: Todo):
-        raise NotImplementedError
+    def create(self, new_todo: Todo) -> Todo:
+        new_todo.id = self._next_id_
+        self._next_id_ += 1
+
+        self.todos.append(new_todo)
+
+        return new_todo
 
     def update(self, todo_id: int, updated_todo: Todo):
-        raise NotImplementedError
+        for index, todo in enumerate(self.todos):
+            if todo.id == todo_id:
+                updated_todo.id = todo_id
+                self.todos[index] = updated_todo
 
-    def delete(self, todo_id: int):
-        raise NotImplementedError
+                return updated_todo
+
+        return None
+
+    def delete(self, todo_id: int) -> bool:
+        for todo in self.todos:
+            if todo.id == todo_id:
+                self.todos.remove(todo)
+
+                return True
+
+        return False
